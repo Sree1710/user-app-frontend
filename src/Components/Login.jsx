f0e9bdb208977c51522175710141c22c73f83c65
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-25 18:02:37 +0530

    Login component tested

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [inputField,setInputField]=useState(
+        {username:"",password:""}
+    )
+
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+    }
+
   return (
-    <div>Login</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <h1>Login</h1>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Username</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="username" value={inputField.username} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input onChange={inputHandler} type="password" name="password" id="" className="form-control" value={inputField.password} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-primary">Sign In</button>
+                        </div>
+                    </div>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"> </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <a href="">New User? Register Here !!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
