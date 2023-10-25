8ceabbd4fbb619205b6564c0018f4255ab8a12c7
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-25 18:53:14 +0530

    UserRegistration component completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import UserRegistration from './Components/UserRegistration';
 
 function App() {
   return (
     <div>
-    <Login/>
+    <UserRegistration/>
     </div>
   );
 }
