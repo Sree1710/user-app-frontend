db8cc06989882f6dea1ddb96bbe4a1f5e646aeef
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-25 21:14:33 +0530

    modifications done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
 import UserRegistration from './Components/UserRegistration';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Login/>} />
+      <Route path='/regu' exact element={<UserRegistration/>} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
