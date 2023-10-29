1551d3d57fa84f4d6f309a58840fb2876943db1e
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-29 12:43:35 +0530

    AddPost component completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Components/Login';
 import UserRegistration from './Components/UserRegistration';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPost from './Components/AddPost';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' exact element={<Login/>} />
       <Route path='/regu' exact element={<UserRegistration/>} />
+      <Route path='/addp' exact element={<AddPost/>}/>
     </Routes>
     </BrowserRouter>
     </div>
