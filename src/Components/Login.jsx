746b627c0187c71f9a8fbbd42290948adebb1dda
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-29 19:23:41 +0530

    NavBar component completed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import AddPost from './AddPost'
+import NavBar from './NavBar'
 
 const Login = () => {
     const [inputField,setInputField]=useState(
@@ -31,6 +32,7 @@ const Login = () => {
 
   return (
     <div>
+        <NavBar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">Post App</span>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default NavBar
